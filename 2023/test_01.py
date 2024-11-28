ebc5a10a2c9499303369d4938e6ee8979a03a28a
Tony Cardella <cardella@gmail.com>
2024-11-27 19:45:19 -0600

    Trying to fix the relative path stuff

--- 2023/test_01.py
+++ 2023/test_01.py
@@ -1,11 +1,16 @@
+import os
 import sys
 
 import pytest
 
 
 class Day01:
+    def __init__(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.input_dir = os.path.join(script_dir, 'inputs/01')
+
     def part1(self, input_file_path: str):
-        with open(input_file_path, 'r') as file:
+        with open(os.path.join(self.input_dir, input_file_path), 'r') as file:
             inputs = file.readlines()
 
         tokens = "1|2|3|4|5|6|7|8|9".split('|')
@@ -22,12 +27,13 @@ class Day01:
         return total_sum
 
     def part2(self, input_file_path: str):
-        with open(input_file_path, 'r') as file:
+        with open(os.path.join(self.input_dir, input_file_path), 'r') as file:
             inputs = file.readlines()
 
         total_sum = 0
 
         tokens = "1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine".split('|')
+        str_to_int_dict = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
 
         for input_line in inputs:
             input_line = input_line.strip()
@@ -52,10 +58,10 @@ class Day01:
                         last_token = token
 
             if not first_token.isdigit():
-                first_token = tokens[first_token]
+                first_token = str_to_int_dict[first_token]
 
             if not last_token.isdigit():
-                last_token = tokens[last_token]
+                last_token = str_to_int_dict[last_token]
 
             value = int(f"{first_token}{last_token}")
             total_sum += value
@@ -64,8 +70,8 @@ class Day01:
 
 
 @pytest.mark.parametrize('input_file_path, expected', [
-    ('inputs/01/example1.txt', 142),
-    ('inputs/01/input.txt', 56465)
+    ('example1.txt', 142),
+    ('input.txt', 56465)
 ])
 def test_part1(input_file_path, expected):
     day01 = Day01()
@@ -74,8 +80,8 @@ def test_part1(input_file_path, expected):
 
 
 @pytest.mark.parametrize('input_file_path, expected', [
-    ('inputs/01/example2.txt', 281),
-    ('inputs/01/input.txt', 55902)
+    ('example2.txt', 281),
+    ('input.txt', 55902)
 ])
 def test_part_2(input_file_path, expected):
     day01 = Day01()
