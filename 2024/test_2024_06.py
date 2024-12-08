c960d3cf9cbc43c3773c5f1ff7cd4a23c1648167
Tony Cardella <cardella@gmail.com>
2024-12-08 11:35:54 -0600

    2024 Day 6 parts 1 and 2 are working

--- 2024/test_2024_06.py
+++ 2024/test_2024_06.py
@@ -3,23 +3,21 @@ import pytest
 
 def parse_input(input_file_path: str):
     with open(input_file_path, 'r', encoding="utf-8-sig") as file:
-        return [line.strip() for line in file.readlines()]
+        stripped_lines = [line.strip() for line in file.readlines()]
+        return [list(row) for row in stripped_lines]
 
 
 def part1(input_file_path: str):
-    map = parse_input(input_file_path)
+    grid = parse_input(input_file_path)
 
-    start = find_start(map)
-    positions = list()
-    positions = [pos[0] for pos in find_path(map, positions, start)]
-    return len(list(set(positions)))
-
+    curr_pos = find_start(grid)
+    path = [pos[0] for pos in find_path(grid, curr_pos, [])]
+    return len(list(set(path)))
 
 
-def find_path(grid, path, start):
-    curr_pos = start
+def find_path(grid, curr_pos, path):
     direction = get_next_direction()
-    path.append((start, direction))
+    path.append((curr_pos, direction))
 
     while is_in_bounds(curr_pos, grid):
         next_pos = (curr_pos[0] + direction[0], curr_pos[1] + direction[1])
@@ -33,27 +31,25 @@ def find_path(grid, path, start):
             direction = get_next_direction()
     return path
 
-def find_loop(grid, path, start, direction = (0,0)):
-    curr_pos = start
-    if direction == (0,0):
-        direction = get_next_direction()
-    path.append((start, direction))
+
+def causes_loop(grid, curr_pos):
+    direction = directions[0]
+    path = set()
 
     while is_in_bounds(curr_pos, grid):
+        if (curr_pos, direction) in path:
+            return True
+        path.add((curr_pos, direction))
+
         next_pos = (curr_pos[0] + direction[0], curr_pos[1] + direction[1])
 
-        if not is_in_bounds(next_pos, grid):
-            return False
-        if not is_obstruction(next_pos, grid):
-            if (next_pos, direction) in path:
-
-                print(path)
-                return True
-            else:
-                path.append((next_pos, direction))
+        if is_in_bounds(next_pos, grid):
+            if not is_obstruction(next_pos, grid):
                 curr_pos = next_pos
+            else:
+                direction = turn_right(direction)
         else:
-            direction = get_next_direction()
+            return False
     return False
 
 
@@ -62,7 +58,7 @@ def find_start(grid):
         for c in range(len(grid[0])):
             if grid[r][c] == '^':
                 return (r, c)
-    return (0,0)
+    return (0, 0)
 
 
 def is_obstruction(pos, map):
@@ -86,49 +82,46 @@ def get_next_direction():
     i += 1
     return dir
 
+
 def turn_right(direction):
     return directions[(directions.index(direction) + 1) % len(directions)]
 
+
 def part2(input_file_path: str):
     grid = parse_input(input_file_path)
-    grid = [list(row) for row in grid]
     count = 0
-    start = find_start(grid)
-    path = find_path(grid, [], start)
-    grid[path[0][0][0]][path[0][0][1]] = '.'
+    curr_pos = find_start(grid)
 
-    for i, curr in enumerate(path):
-        nx = curr[0][0] + curr[1][0]
-        ny = curr[0][1] + curr[1][1]
-        next_pos = (nx, ny)
+    empty_positions = []
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == '.':
+                empty_positions.append((r,c))
 
-        if is_in_bounds(next_pos, grid):
-            if is_obstruction(next_pos, grid):
-                continue
-            else:
-                grid[nx][ny] = '#'
-                if find_loop(grid, path[:i], curr[0], curr[1]):
-                    print(f"(nx,ny) = ({nx},{ny})")
-                    count+=1
-                grid[nx][ny] = '.'
-        else:
-            break
+    for r,c in empty_positions:
+        grid[r][c] = '#'
+
+        if causes_loop(grid, curr_pos):
+            count+=1
+
+        grid[r][c] = '.'
 
     return count
 
-# @pytest.mark.parametrize('input_file_path, expected', [
-#     ('inputs/06/example.txt', 41),
-#     ('inputs/06/input.txt', 5318)
-# ])
-# def test_part_1(input_file_path, expected):
-#     actual = part1(input_file_path)
-#     assert actual == expected
-#
-#
-# @pytest.mark.parametrize('input_file_path, expected', [
-#     ('inputs/06/example.txt', 6),
-#     #('inputs/06/input.txt', 5346)
-# ])
-# def test_part_2(input_file_path, expected):
-#     actual = part2(input_file_path)
-#     assert actual == expected
+
+@pytest.mark.parametrize('input_file_path, expected', [
+    ('inputs/06/example.txt', 41),
+    ('inputs/06/input.txt', 5318)
+])
+def test_part_1(input_file_path, expected):
+    actual = part1(input_file_path)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('input_file_path, expected', [
+    ('inputs/06/example.txt', 6),
+    ('inputs/06/input.txt', 5346)
+])
+def test_part_2(input_file_path, expected):
+    actual = part2(input_file_path)
+    assert actual == expected
