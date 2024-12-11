be2d4087fe78b4ca74caf7982c2db3f45a0acb56
Tony Cardella <cardella@gmail.com>
2024-12-11 07:29:33 -0600

    2024 Day 11 part 1 working (brute force), part 2 WIP (brute force don't work)

---
+++ 2024/test_2024_11.py
@@ -0,0 +1,137 @@
+from collections import Counter
+
+import pytest
+
+
+def parse_input(input_file_path: str):
+    with open(input_file_path, 'r', encoding="utf-8-sig") as file:
+        return list(map(int, file.read().split()))
+        #return list(file.read().split())
+
+
+def part1(input_file_path: str):
+    stones = parse_input(input_file_path)
+
+    for i in range(25):
+        output = []
+
+        for i, stone in enumerate(stones):
+
+            if (stone == 0):
+                output.append(1)
+            elif len(str(stone)) % 2 == 0:
+                stone_string = str(stone)
+                midpoint = int(len(stone_string) / 2)
+
+                output.append(int(stone_string[:midpoint]))
+                output.append(int(stone_string[midpoint:]))
+            else:
+                stone *= 2024
+                output.append(stone)
+
+        stones = output
+
+    return len(stones)
+
+
+def part2(input_file_path: str):
+    stones = parse_input(input_file_path)
+
+    stones = [0]
+    stone_counts = {0: 1}
+    i = 0
+    while i < 75:
+        output = []
+
+        for stone in stones:
+
+            if (stone == 0):
+                output.append(1)
+            elif len(str(stone)) % 2 == 0:
+                stone_string = str(stone)
+                midpoint = int(len(stone_string) / 2)
+
+                output.append(int(stone_string[:midpoint]))
+                output.append(int(stone_string[midpoint:]))
+            else:
+                stone *= 2024
+                output.append(stone)
+
+        stone_counts[i] = len(output)
+        stones = output
+
+        i+=1
+
+    print(stone_counts)
+    return len(stones)
+
+def part2a(input_file_path: str):
+    stones = parse_input(input_file_path)
+
+    count = Counter(len(str(stone)) for stone in stones)
+
+    for _ in range(75):
+        new_count = Counter()
+
+        for digit_len, num_stones in count.items():
+            if digit_len == 1:
+                new_count[1] += num_stones
+            elif digit_len % 2 == 0:
+                new_len = digit_len // 2
+                new_count[new_len] += 2* num_stones
+            else:
+                prod_len = len(str(2024 * (10 ** (digit_len - 1))))
+                new_count[prod_len] += num_stones
+
+        count = new_count
+
+    return sum(count.values())
+
+def part2b(input_file_path: str):
+    stones = parse_input(input_file_path)
+
+    state_counts = Counter()
+
+    # Initialize states (group by digit length)
+    for stone in stones:
+        length = len(str(stone))
+        state_counts[length] += 1
+
+    for _ in range(75):
+        new_state_counts = Counter()
+
+        for digit_len, count in state_counts.items():
+            if digit_len % 2 == 0:
+                # Even-digit stones split into two new stones
+                new_length = digit_len // 2
+                new_state_counts[new_length] += 2 * count
+            else:
+                # Odd-digit stones grow
+                new_length = len(str(2024 * (10 ** (digit_len - 1))))
+                new_state_counts[new_length] += count
+
+        # Update the states for the next blink
+        state_counts = new_state_counts
+
+    # Compute the total number of stones after all blinks
+    total_stones = sum(state_counts.values())
+    return total_stones
+
+@pytest.mark.parametrize('input_file_path, expected', [
+    #('inputs/11/example1.txt', 36),
+    ('inputs/11/example2.txt', 55312),
+    ('inputs/11/input.txt', 216996)
+])
+def test_part_1(input_file_path, expected):
+    actual = part1(input_file_path)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('input_file_path, expected', [
+    # ('inputs/11/example1.txt', 81),
+    # ('inputs/11/example2.txt', 81),
+     #('inputs/11/input.txt', 1116)
+])
+def test_part_2(input_file_path, expected):
+    actual = part2(input_file_path)
+    assert actual == expected
