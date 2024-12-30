c4db973707dbb8cc96e1d646b1cb5ea466d75332
Tony Cardella <cardella@gmail.com>
2024-12-29 21:05:14 -0600

    2024 Day 13, now working for part 1 and 2

--- 2024/test_2024_13.py
+++ 2024/test_2024_13.py
@@ -1,4 +1,4 @@
-from math import gcd
+import re
 
 import pytest
 
@@ -6,108 +6,49 @@ import pytest
 def parse_input(input_file_path: str):
     output = []
     with open(input_file_path, 'r', encoding="utf-8-sig") as file:
-        text = file.read()
-        machines = text.split('\n\n')
-        for machine in machines:
-            lines = machine.split('\n')
-            output.append([split_button_line(lines[0]), split_button_line(lines[1]), split_prize_line(lines[2])])
-
+        for block in file.read().split('\n\n'):
+            output.append(map(int, re.findall(r'\d+', block)))
     return output
 
 
-def split_button_line(input):
-    x_and_y = input.split(':')[1]
-    x, y = x_and_y.split(',')
-    return int(x.split('+')[1]), int(y.split('+')[1])
-
-
-def split_prize_line(input):
-    x_and_y = input.split(':')[1]
-    x, y = x_and_y.split(',')
-    return int(x.split('=')[1]), int(y.split('=')[1])
-
-
 def part1(input_file_path: str):
-    inputs = parse_input(input_file_path)
-    total_tokens = 0
-    prize_count = 0
+    total = 0
+    machines = parse_input(input_file_path)
+    for machine in machines:
+        ax, ay, bx, by, px, py = machine
 
-    for machine in inputs:
-        delta_a, delta_b, prize = machine
-        result = solve_machine(delta_a, delta_b, prize)
+        # solving for ca and cb in these equations:
+        #   ax * ca + bx * cb == px
+        #   AND
+        #   ay * ca + by * cb == py
+        ca = (px * by - py * bx) / (ax * by - ay * bx)
+        cb = (px - ax * ca) / bx
 
-        if result is not None:
-            total_tokens += result
-            prize_count += 1
+        if ca % 1 == cb % 1 == 0:  # is int?
+            total += int(ca * 3 + cb)
 
-    return total_tokens
-
-
-def solve_machine(delta_a, delta_b, prize):
-    dx_a, dy_a = delta_a
-    dx_b, dy_b = delta_b
-    px, py = prize
-
-    min_tokens = float("inf")
-
-    for a in range(101):
-        for b in range(101):
-            x_pos = a * dx_a + b * dx_b
-            y_pos = a * dy_a + b * dy_b
-
-            if x_pos == px and y_pos == py:
-                cost = 3 * a + b
-                min_tokens = min(min_tokens, cost)
-
-    return min_tokens if min_tokens != float("inf") else None
-
-
-def extended_euclidean(a, b):
-    if b == 0:
-        return (1, 0)
-    x, y = extended_euclidean(b, a % b)
-    return (y, x - (a // b) * y)
-
-
-def solve_diophantine(delta_a, delta_b, prize):
-    dx_a, dy_a = delta_a
-    dx_b, dy_b = delta_b
-    px, py = prize
-
-    g = gcd(dx_a, dx_b)
-    if px % g != 0:
-        return None
-
-    scaled_dx_a, scaled_dx_b, scaled_px = dx_a // g, dx_b // g, px // g
-    x, y = extended_euclidean(scaled_dx_a, scaled_dx_b)
-
-    x *= scaled_px
-    y *= scaled_px
-
-    results = []
-    results.append(solve_machine(x, y, (px,py)))
-    return results
-    # TODO: A miracle happens
-
-    return None
+    return total
 
 
 def part2(input_file_path: str):
-    inputs = parse_input(input_file_path)
-    total_tokens = 0
-    reachable_prizes = 0
+    total = 0
+    machines = parse_input(input_file_path)
+    for machine in machines:
+        ax, ay, bx, by, px, py = machine
+        px += 10000000000000
+        py += 10000000000000
 
-    for machine in inputs:
-        delta_a, delta_b, prize = machine
-        shifted_prize = (prize[0]+ 10 ** 13, prize[1] + 10 ** 13)
+        # solving for i and j in this equation:
+        #   ax * ca + bx * cb == px
+        #   AND
+        #   ay * ca + by * cb == py
+        ca = (px * by - py * bx) / (ax * by - ay * bx)
+        cb = (px - ax * ca) / bx
 
-        result = solve_diophantine(delta_a, delta_b, shifted_prize)
+        if ca % 1 == cb % 1 == 0:  # is int?
+            total += int(ca * 3 + cb)
 
-        if result is not None:
-            total_tokens += result
-            reachable_prizes += 1
-
-    return total_tokens
+    return total
 
 
 @pytest.mark.parametrize('input_file_path, expected', [
@@ -120,8 +61,7 @@ def test_part_1(input_file_path, expected):
 
 
 @pytest.mark.parametrize('input_file_path, expected', [
-    # ('inputs/13/example.txt', 81),
-    # ('inputs/13/input.txt', 1116)
+    ('inputs/13/input.txt', 106228669504887)
 ])
 def test_part_2(input_file_path, expected):
     actual = part2(input_file_path)
