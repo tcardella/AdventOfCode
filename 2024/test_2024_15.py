4ae5546d85e27700bdc3d2d66b3a3ea34d02d620
Tony Cardella <cardella@gmail.com>
2024-12-30 23:57:08 -0600

    2024 Day 15, parts 1 and 2 now working + refactor and clean-up

--- 2024/test_2024_15.py
+++ 2024/test_2024_15.py
@@ -20,104 +20,9 @@ def find_start(grid):
                 return r, c
 
 
-def is_against_wall(grid, r, c, dr, dc, box_size=1):
-    nr, nc = r + dr, c + dc
-    if grid[nr][nc] == '#':
-        return True
-    elif grid[nr][nc] == '.':
-        return False
-    elif box_size == 1:
-        if grid[nr][nc] == 'O':
-            return is_against_wall(grid, nr, nc, dr, dc, box_size)
-    elif box_size == 2:
-        return is_against_wall(grid, nr, nc, dr, dc, box_size)
-    else:
-        return False
-
-
-def can_move(grid, r, c, dr, dc, box_size=1):
-    nr, nc = r + dr, c + dc
-
-    target = grid[nr][nc]
-    if target == '.':
-        return True
-    if target == '#':
-        return False
-    # if box_size == 1 and target == 'O':
-    if target == 'O':
-        if not is_against_wall(grid, nr, nc, dr, dc):
-            return True
-    # elif box_size == 2 and (target == '[' or target == ']'):
-    #     if not is_against_wall(grid, nr, nc, dr, dc) and not is_against_wall(grid, nr + dr, nc + dc, dr, dc):
-    #         return True
-    if target == '[':
-        if dc == 1 or dc == -1:  # from the left or right
-            if not is_against_wall(grid, nr, nc, dr, dc) and not is_against_wall(grid, nr + dr, nc + dc, dr, dc):
-                return True
-        if dr == 1:
-            if not is_against_wall(grid, nr, nc, dr, dc) and not is_against_wall(grid, nr, nc + 1, dr, dc):
-                return True
-        if dr == -1:
-            if not is_against_wall(grid, nr, nc, dr, dc) and not is_against_wall(grid, nr, nc + 1, dr, dc):
-                return True
-    if target == ']':
-        if dc == 1 or dc == -1:  # from the left or right
-            if not is_against_wall(grid, nr, nc, dr, dc) and not is_against_wall(grid, nr + dr, nc + dc, dr, dc):
-                return True
-        if dr == 1:
-            if not is_against_wall(grid, nr, nc-1, dr, dc) and not is_against_wall(grid, nr, nc, dr, dc):
-                return True
-        if dr == -1:
-            if not is_against_wall(grid, nr, nc-1, dr, dc) and not is_against_wall(grid, nr, nc, dr, dc):
-                return True
-
-    return False
-
-
-def swap(grid, sr, sc, tr, tc, swap_size=1):
-    temp = grid[sr][sc]
-    grid[sr][sc] = grid[tr][tc]
-    grid[tr][tc] = temp
-
-
-def swap2(grid, s1r, s1c, s2r, s2c, t1r, t1c, t2r, t2c):
-    temp1 = grid[s1r][s1c]
-    temp2 = grid[s2r][s2c]
-    grid[s1r][s1c] = grid[t1r][t1c]
-    grid[s2r][s2c] = grid[t2r][t2c]
-    grid[t1r][t1c] = temp1
-    grid[t2r][t2c] = temp2
-
-
-def make_move(grid, r, c, dr, dc, box_size=1):
-    nr, nc = r + dr, c + dc
-
-    if grid[nr][nc] == '.':
-        swap(grid, r, c, nr, nc)
-        return nr, nc
-    elif box_size == 1:
-        if grid[nr][nc] == 'O':
-            make_move(grid, nr, nc, dr, dc)
-            swap(grid, r, c, nr, nc)
-            return nr, nc
-        else:
-            return r, c
-    elif box_size == 2:
-        if grid[nr][nc] == '[' or grid[nr][nc] == ']':
-            make_move(grid, nr, nc, dr, dc, 2)
-            swap(grid, r, c, nr, nc)
-            return nr, nc
-        else:
-            return r, c
-
-
-def dump_grid(grid):
-    for i, row in enumerate(grid):
-        print(''.join(row))
-
-
 def part1(input_file_path: str):
     grid, moves, start = parse_input(input_file_path)
+    rows, cols = len(grid), len(grid[0])
 
     directions = {
         '^': (-1, 0),
@@ -129,16 +34,34 @@ def part1(input_file_path: str):
 
     for move in moves:
         dr, dc = directions[move]
-        if can_move(grid, r, c, dr, dc):
-            r, c = make_move(grid, r, c, dr, dc)
+        targets = [(r, c)]
+        cr = r
+        cc = c
+        can_move = True
+        while True:
+            cr += dr
+            cc += dc
+            e = grid[cr][cc]
+            if e == '#':
+                can_move = False
+                break
+            elif e == 'O':
+                targets.append((cr, cc))
+            elif e == '.':
+                break
 
-    gps_sum = 0
-    for r in range(len(grid)):
-        for c in range(len(grid[r])):
-            if grid[r][c] == 'O':
-                gps_sum += 100 * r + c
+        if not can_move:
+            continue
 
-    return gps_sum
+        grid[r][c] = '.'  # move the robot from its original spot
+        grid[r + dr][c + dc] = '@'  # move the robot to its new spot
+        for br, bc in targets[1:]:  # first element is the robot
+            grid[br + dr][bc + dc] = 'O'
+
+        r += dr
+        c += dc
+
+    return sum(100 * r + c for r in range(rows) for c in range(cols) if grid[r][c] == 'O')
 
 
 def expand_grid(grid):
@@ -161,6 +84,7 @@ def expand_grid(grid):
 def part2(input_file_path: str):
     grid, moves, start = parse_input(input_file_path)
     grid = expand_grid(grid)
+    rows, cols = len(grid), len(grid[0])
     start = find_start(grid)
 
     directions = {
@@ -172,20 +96,39 @@ def part2(input_file_path: str):
     r, c = start
 
     for move in moves:
-        print()
-        dump_grid(grid)
         dr, dc = directions[move]
-        if can_move(grid, r, c, dr, dc, 2):
-            r, c = make_move(grid, r, c, dr, dc, 2)
-    print()
-    dump_grid(grid)
-    gps_sum = 0
-    for r in range(len(grid)):
-        for c in range(len(grid[r])):
-            if grid[r][c] == '[':
-                gps_sum += 100 * r + c
+        targets = [(r, c)]
+        can_move = True
+        for cr, cc in targets:
+            nr = cr + dr
+            nc = cc + dc
+            if (nr, nc) in targets:
+                continue
+            e = grid[nr][nc]
+            if e == '#':
+                can_move = False
+                break
+            elif e == '[':  # if one of the halves is found, add the other half
+                targets.append((nr, nc))
+                targets.append((nr, nc + 1))
+            elif e == ']':  # if one of the halves is found, add the other half
+                targets.append((nr, nc))
+                targets.append((nr, nc - 1))
+        if not can_move:
+            continue
 
-    return gps_sum
+        copy = [list(row) for row in grid]  # deepcopy
+        grid[r][c] = '.'  # move the robot from its original spot
+        grid[r + dr][c + dc] = '@'  # move the robot to its new spot
+        for br, bc in targets[1:]:  # first element is the robot
+            grid[br][bc] = '.'
+        for br, bc in targets[1:]:  # first element is the robot
+            grid[br + dr][bc + dc] = copy[br][bc]
+
+        r += dr
+        c += dc
+
+    return sum(100 * r + c for r in range(rows) for c in range(cols) if grid[r][c] == '[')
 
 
 @pytest.mark.parametrize('input_file_path, expected', [
@@ -199,11 +142,10 @@ def test_part_1(input_file_path, expected):
 
 
 @pytest.mark.parametrize('input_file_path, expected', [
-    # TODO: Fix these tests
-    # ('inputs/15/example0.txt', 9021),
-    # ('inputs/15/example1.txt', 9021),
-    #('inputs/15/example2.txt', 9021),
-    # ('inputs/15/input.txt', 6576)
+    ('inputs/15/example0.txt', 1751),
+    ('inputs/15/example1.txt', 9021),
+    ('inputs/15/example2.txt', 618),
+    ('inputs/15/input.txt', 1519991)
 ])
 def test_part_2(input_file_path, expected):
     actual = part2(input_file_path)
